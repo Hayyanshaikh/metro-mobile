b478ba95603af116a0b4c9aeaf92dfe4a36bc3c0
hayyanshaikh <hayyanshaikh55@gmail.com>
2024-09-06 21:12:40 +0500

    Complete 2 pages

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@ $(function() {
   $(window).resize(function() {
     convertToOffcanvas();
   }).trigger('resize');
+
+  var swiper = new Swiper(".product_slider", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".deal_section .swiper-button-next",
+      prevEl: ".deal_section .swiper-button-prev",
+    },
+  });var swiper = new Swiper(".benifit_product_slider", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".benefits .swiper-button-next",
+      prevEl: ".benefits .swiper-button-prev",
+    },
+  });
 });
 
 const convertToOffcanvas = () => {
